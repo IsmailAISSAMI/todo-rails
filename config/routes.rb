33ee0000e610b3a63c7685513415a948192cb33c
ismail aissami <ismail.aissami@ens.uvsq.fr>
2020-12-04 23:54:48 +0100

    :rocket: scaffold tasks and deploy in heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  
+  # Home page is task's index for the moment 
+  root "tasks#index"
+  # All Task's links  		
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
